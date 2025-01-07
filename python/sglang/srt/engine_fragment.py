472fd4a1aa0c3d984c47e7f4e7753de9e7e2002e
fzyzcjy <ch271828n@outlook.com>
2025-01-07 13:18:24 +0800

    more

--- python/sglang/srt/engine_fragment.py
+++ python/sglang/srt/engine_fragment.py
@@ -1,3 +1,19 @@
+import multiprocessing as mp
+
+from sglang.srt.managers.scheduler import run_scheduler_process
+
+
 class EngineFragment:
     def __init__(self):
-        TODO
+        self._proc = mp.Process(
+            target=run_scheduler_process,
+            kwargs=dict(
+                server_args=TODO,
+                port_args=TODO,
+                gpu_id=TODO,
+                tp_rank=TODO,
+                dp_rank=TODO,
+                ready_ipc_name=TODO,
+            ),
+        )
+        self._proc.start()
