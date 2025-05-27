911f01b34677f3df7293cb7ad7b7edc597a8e439
fzyzcjy <ch271828n@outlook.com>
2025-05-27 10:56:21 +0800

    SGLANG_MOONCAKE_THREAD_POOL_SIZE

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -29,7 +29,12 @@ from sglang.srt.disaggregation.base.conn import (
 from sglang.srt.disaggregation.mooncake.transfer_engine import MooncakeTransferEngine
 from sglang.srt.disaggregation.utils import DisaggregationMode
 from sglang.srt.server_args import ServerArgs
-from sglang.srt.utils import get_free_port, get_ip, get_local_ip_by_remote
+from sglang.srt.utils import (
+    get_free_port,
+    get_int_env_var,
+    get_ip,
+    get_local_ip_by_remote,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -155,10 +160,10 @@ class MooncakeKVManager(BaseKVManager):
             self._register_to_bootstrap()
 
             # Determine the number of threads to use for kv sender
-            cpu_count = os.cpu_count()
-            self.executor = concurrent.futures.ThreadPoolExecutor(
-                min(cpu_count // 4, 16)
+            max_workers = get_int_env_var(
+                "SGLANG_MOONCAKE_THREAD_POOL_SIZE", min(os.cpu_count() // 4, 16)
             )
+            self.executor = concurrent.futures.ThreadPoolExecutor(max_workers)
         elif self.disaggregation_mode == DisaggregationMode.DECODE:
             self.start_decode_thread()
             self.connection_pool: Dict[str, Dict[str, Union[str, int]]] = {}
