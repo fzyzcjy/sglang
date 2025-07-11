7e638837a36d19b9297c35b839ffd2c6ca73d395
fzyzcjy <ch271828n@outlook.com>
2025-07-11 14:48:13 +0800

    more

--- python/sglang/srt/debug_utils/answer_comparator.py
+++ python/sglang/srt/debug_utils/answer_comparator.py
@@ -1,12 +1,22 @@
+import polars as pl
 import argparse
-from typing import List
 
 
 def main(args):
-    baseline_paths: List[str] = args.baseline_path
-    target_paths: List[str] = args.target_path
+    df = pl.concat([
+        _read_df_raw(p, category=category, trial_index=i)
+        for category, paths in [
+            ("baseline", args.baseline_path),
+            ("target", args.target_path),
+        ]
+        for i, p in paths
+    ])
 
     TODO
+
+
+def _read_df_raw(path: str, category: str, trial_index: int):
+    return pl.read_json(path).with_columns(category=pl.lit(category), trial_index=trial_index)
 
 
 if __name__ == '__main__':
