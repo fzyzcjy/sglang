6d5544e6f548a352cb1f4232acc7ed2cd1abcfec
fzyzcjy <ch271828n@outlook.com>
2025-07-11 14:44:43 +0800

    more

--- python/sglang/srt/debug_utils/answer_comparator.py
+++ python/sglang/srt/debug_utils/answer_comparator.py
@@ -1,12 +1,17 @@
 import argparse
+from typing import List
+
 
 def main(args):
+    baseline_paths: List[str] = args.baseline_path
+    target_paths: List[str] = args.target_path
+
     TODO
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--baseline", type=str, nargs="+")
-    parser.add_argument("--target", type=str, nargs="+")
+    parser.add_argument("--baseline-path", type=str, nargs="+")
+    parser.add_argument("--target-path", type=str, nargs="+")
     args = parser.parse_args()
     main(args)
