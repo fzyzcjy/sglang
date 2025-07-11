97b79b29541a3f3f100d1482da654eee81826aa9
fzyzcjy <ch271828n@outlook.com>
2025-07-11 14:59:35 +0800

    more

--- python/sglang/srt/debug_utils/answer_comparator.py
+++ python/sglang/srt/debug_utils/answer_comparator.py
@@ -3,28 +3,10 @@ import argparse
 
 
 def main(args):
-    df_input = pl.concat([
-        _read_df_raw(p, category=category, trial_index=i)
-        for category, paths in [
-            ("baseline", args.baseline_path),
-            ("target", args.target_path),
-        ]
-        for i, p in paths
-    ])
+    df_input = _compute_df_input(args)
     assert all(c in df_input.columns for c in ["category", "trial_index", "prompt_id", "prompt", "answer", "correct"])
 
-    df_meta = pl.DataFrame([
-        dict(
-            prompt_id=prompt_id,
-            **_handle_one_prompt(df_input.filter(pl.col("prompt_id") == prompt_id)),
-        )
-        for prompt_id in sorted(df_input["prompt_id"].to_list())
-    ])
-
-    df_meta = df_meta.with_columns(
-        correctness_delta=pl.col("correctness_target") - pl.col("correctness_baseline"),
-    )
-    df_meta = df_meta.sort("correctness_delta", "answer_same_prefix_len")
+    df_meta = _compute_df_meta(df_input)
 
     df_correctness_delta = df_meta.group_by("correctness_delta").count().sort("correctness_delta")
 
@@ -35,8 +17,34 @@ def main(args):
     TODO
 
 
+def _compute_df_input(args):
+    return pl.concat([
+        _read_df_raw(p, category=category, trial_index=i)
+        for category, paths in [
+            ("baseline", args.baseline_path),
+            ("target", args.target_path),
+        ]
+        for i, p in paths
+    ])
+
+
 def _read_df_raw(path: str, category: str, trial_index: int):
     return pl.read_json(path).with_columns(category=pl.lit(category), trial_index=trial_index)
+
+
+def _compute_df_meta(df_input: pl.DataFrame):
+    df_meta = pl.DataFrame([
+        dict(
+            prompt_id=prompt_id,
+            **_handle_one_prompt(df_input.filter(pl.col("prompt_id") == prompt_id)),
+        )
+        for prompt_id in sorted(df_input["prompt_id"].to_list())
+    ])
+    df_meta = df_meta.with_columns(
+        correctness_delta=pl.col("correctness_target") - pl.col("correctness_baseline"),
+    )
+    df_meta = df_meta.sort("correctness_delta", "answer_same_prefix_len")
+    return df_meta
 
 
 def _handle_one_prompt(df_one_prompt: pl.DataFrame):
