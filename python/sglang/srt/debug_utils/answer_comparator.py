408426b9fd10befe533e53e6850bafc4cf634df9
fzyzcjy <ch271828n@outlook.com>
2025-07-11 14:44:04 +0800

    more

--- python/sglang/srt/debug_utils/answer_comparator.py
+++ python/sglang/srt/debug_utils/answer_comparator.py
@@ -1,6 +1,12 @@
-def run():
+import argparse
+
+def main(args):
     TODO
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--baseline", type=str, nargs="+")
+    parser.add_argument("--target", type=str, nargs="+")
+    args = parser.parse_args()
+    main(args)
