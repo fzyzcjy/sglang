097ecb686d4aba33c60ba70c3dbf40db32811ab6
fzyzcjy <ch271828n@outlook.com>
2025-07-28 09:29:53 +0800

    more
    (cherry picked from commit e7515eb4b73ae3b9b101eef4f7cdc6ec9d0d3e37)

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1982,6 +1982,7 @@ class Scheduler(
             deepep_mode=DeepEPMode[self.server_args.deepep_mode],
             require_mlp_tp_gather=require_mlp_tp_gather(self.server_args),
             disable_overlap_schedule=self.server_args.disable_overlap_schedule,
+            enable_dp_attention=self.server_args.enable_dp_attention,
         )
 
     @staticmethod
@@ -1999,6 +2000,7 @@ class Scheduler(
         deepep_mode: DeepEPMode,
         require_mlp_tp_gather: bool,
         disable_overlap_schedule: bool,
+        enable_dp_attention: bool,
     ):
         # Check if other DP workers have running batches
         if local_batch is None:
@@ -2053,7 +2055,7 @@ class Scheduler(
             device=device,
         )
         global_info = torch.empty(
-            (dp_size, attn_tp_size, 6),
+            (dp_size if enable_dp_attention else 1, attn_tp_size, 6),
             dtype=torch.int64,
             device=device,
         )
