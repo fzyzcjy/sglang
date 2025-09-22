459ac8c8ea7a6436383d0c119b20b2b549a4dddd
fzyzcjy <ch271828n@outlook.com>
2025-09-22 21:44:04 +0800

    Revert "hack: temp make q fp8 (logic is incorrect)"
    This reverts commit 04d17fc0e9523c6fc534e01d054eb9195a793a15.

--- python/sglang/srt/layers/attention/trtllm_mla_backend.py
+++ python/sglang/srt/layers/attention/trtllm_mla_backend.py
@@ -629,9 +629,8 @@ class TRTLLMMLABackend(FlashInferMLAAttnBackend):
             )
             max_seq_len = metadata.max_seq_len + forward_batch.spec_info.draft_token_num
 
-            assert kv_cache.dtype == torch.float8_e4m3fn
             raw_out = flashinfer.decode.trtllm_batch_decode_with_kv_cache_mla(
-                query=q.to(torch.float8_e4m3fn),
+                query=q,
                 kv_cache=kv_cache,
                 workspace_buffer=self.workspace_buffer,
                 qk_nope_head_dim=self.qk_nope_head_dim,
