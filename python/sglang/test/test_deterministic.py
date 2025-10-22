de412f04ee903cac07f3740ab2e70f022a5ba377
fzyzcjy <ch271828n@outlook.com>
2025-10-22 10:17:42 +0800

    enhance p_vs_d test

--- python/sglang/test/test_deterministic.py
+++ python/sglang/test/test_deterministic.py
@@ -103,6 +103,7 @@ def send_single(
     prompt: List[str] = None,
     max_new_tokens: int = None,
     extra_params: Optional[Dict[str, Any]] = None,
+    pick_first_result: bool = True,
 ):
     base_url = f"http://{args.host}:{args.port}"
 
@@ -174,7 +175,8 @@ def send_single(
     else:
         ret = response.json()
 
-    ret = ret[0] if isinstance(ret, list) else ret
+    if pick_first_result:
+        ret = ret[0] if isinstance(ret, list) else ret
 
     if return_full_response:
         return ret
@@ -224,24 +226,40 @@ def send_prefix(args, batch_size: int, prompts: List[str]):
     return ret_dict
 
 
-def _test_mode_p_vs_d(args):
+def _test_mode_p_vs_d(args, batch_size):
+    print()
+    print(f"Execute: test p_vs_d {batch_size=}")
+
+    random.seed(42)
     args.return_logprob = True
     query_extra_params = {
         "logprob_start_len": 0,
         "return_text_in_logprobs": True,
     }
 
-    prompt = PROMPT_1
+    def _create_prompts():
+        ans = [PROMPT_1, PROMPT_2]
+        for i in range(batch_size - len(ans)):
+            end = random.randrange(1, 4096)
+            if random.random() < 0.5:
+                begin = 0
+            else:
+                begin = random.randrange(0, end)
+            ans.append(LONG_PROMPT[begin:end])
+        return ans[:batch_size]
 
     # warmup + flush
     send_single(args, input_ids=[1] * 64, max_new_tokens=65, return_full_response=True)
     requests.post(f"http://{args.host}:{args.port}/flush_cache")
 
+    prompts = _create_prompts()
+
     resp_a = send_single(
         args,
-        prompt=[prompt],
+        prompt=prompts,
         max_new_tokens=args.max_new_tokens,
         return_full_response=True,
+        pick_first_result=False,
         extra_params=query_extra_params,
     )
     info_a = _extract_ids_and_logprobs(resp_a)
@@ -250,16 +268,21 @@ def _test_mode_p_vs_d(args):
 
     resp_b = send_single(
         args,
-        input_ids=info_a["io"].token_ids,
+        input_ids=[x["io"].token_ids for x in info_a],
         max_new_tokens=1,
         return_full_response=True,
+        pick_first_result=False,
         extra_params=query_extra_params,
     )
     info_b = _extract_ids_and_logprobs(resp_b)
 
-    correct = TokenIdsAndLogprobs.compare(info_a["io"], info_b["input"])
+    ans = []
+    for i, (info_a_item, info_b_item) in enumerate(zip(info_a, info_b, strict=True)):
+        print(f"Compare sequence {i} in batch...")
+        correct = TokenIdsAndLogprobs.compare(info_a_item["io"], info_b_item["input"])
+        ans.append(int(correct))
 
-    return [int(correct)]
+    return ans
 
 
 @dataclasses.dataclass
@@ -282,12 +305,12 @@ class TokenIdsAndLogprobs:
         if token_match:
             print(f"Token match: {a.token_ids}")
         else:
-            print(f"Token mismatch: {a.token_ids=} {b.token_ids=}")
+            print(f"❗Token mismatch: {a.token_ids=} {b.token_ids=}")
 
         if logprobs_match:
             print(f"Logprobs match:", a.logprobs)
         else:
-            print(f"Logprobs mismatch")
+            print(f"❗Logprobs mismatch")
             print(
                 "    A:   ",
                 [f"{x:.10f}" if x is not None else "None" for x in a.logprobs],
@@ -305,8 +328,8 @@ class TokenIdsAndLogprobs:
         return token_match and logprobs_match
 
 
-def _extract_ids_and_logprobs(response):
-    def _extract_part(name):
+def _extract_ids_and_logprobs(responses):
+    def _extract_part(response, name):
         token_ids, logprobs = [], []
         for item in response["meta_info"][name]:
             logprob, token_id, text = item
@@ -314,9 +337,14 @@ def _extract_ids_and_logprobs(response):
             logprobs.append(logprob)
         return TokenIdsAndLogprobs(token_ids=token_ids, logprobs=logprobs)
 
-    input = _extract_part("input_token_logprobs")
-    output = _extract_part("output_token_logprobs")
-    return dict(input=input, output=output, io=input + output)
+    def _extract_one_response(response):
+        input = _extract_part(response, "input_token_logprobs")
+        output = _extract_part(response, "output_token_logprobs")
+        return dict(input=input, output=output, io=input + output)
+
+    if not isinstance(responses, list):
+        responses = [responses]
+    return [_extract_one_response(x) for x in responses]
 
 
 def test_deterministic(args):
@@ -517,7 +545,10 @@ def test_deterministic(args):
 
     elif args.test_mode == "p_vs_d":
         # TODO also extract other modes to functions
-        return _test_mode_p_vs_d(args)
+        ans = []
+        for i in range(1, args.n_trials + 1):
+            ans += _test_mode_p_vs_d(args, batch_size=i)
+        return ans
 
     else:
         raise ValueError(f"Invalid test mode: {args.test_mode}")
