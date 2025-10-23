b9aa2f03814639ad80d9f06bdcff21852ca66dcf
fzyzcjy <ch271828n@outlook.com>
2025-10-23 16:30:45 +0800

    dump

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -53,6 +53,7 @@ from sglang.srt.utils import (
     is_sm100_supported,
     prepare_weight_cache,
 )
+from sglang.srt.debug_utils.dumper import dumper
 
 _is_flashinfer_available = is_flashinfer_available()
 _is_sm90_supported = is_cuda() and is_sm90_supported()
@@ -223,6 +224,7 @@ class LayerCommunicator:
         forward_batch: ForwardBatch,
         qaunt_format: str = "",
     ):
+        dumper.set_ctx(norm_mode="input_ln")
         if hidden_states.shape[0] == 0:
             residual = hidden_states
         else:
@@ -267,6 +269,7 @@ class LayerCommunicator:
                         hidden_states, residual = self.input_layernorm(
                             hidden_states, residual
                         )
+        dumper.set_ctx(norm_mode=None)
 
         hidden_states = self._communicate_simple_fn(
             hidden_states=hidden_states,
@@ -286,13 +289,16 @@ class LayerCommunicator:
         if cache is not None:
             self._context.cache = cache
 
-        return self._communicate_with_all_reduce_and_layer_norm_fn(
+        dumper.set_ctx(norm_mode="post_attn_ln")
+        ans = self._communicate_with_all_reduce_and_layer_norm_fn(
             hidden_states=hidden_states,
             residual=residual,
             forward_batch=forward_batch,
             layernorm=self.post_attention_layernorm,
             context=self._context,
         )
+        dumper.set_ctx(norm_mode=None)
+        return ans
 
     def postprocess_layer(
         self,
