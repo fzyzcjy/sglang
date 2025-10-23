ce563e9e39251baa7fa37fd6e66913fc82c0dcb5
fzyzcjy <ch271828n@outlook.com>
2025-10-23 18:13:53 +0800

    dump

--- python/sglang/srt/debug_utils/dump_comparator.py
+++ python/sglang/srt/debug_utils/dump_comparator.py
@@ -160,14 +160,14 @@ def _get_tensor_dim_descs():
             target_desc="num_tokens (num_heads head_dim)",
         ),
         dict(
-            pattern="rmsnorm.*hidden.*(input_ln|post_attn_ln)",
-            baseline_desc="1 num_tokens head_dim",
-            target_desc="num_tokens head_dim",
+            pattern="rmsnorm.*hidden.*mode=(q|k)",
+            baseline_desc="1 num_tokens num_heads head_dim",
+            target_desc="(num_tokens num_heads) head_dim",
         ),
         dict(
             pattern="rmsnorm.*hidden",
-            baseline_desc="1 num_tokens num_heads head_dim",
-            target_desc="(num_tokens num_heads) head_dim",
+            baseline_desc="1 num_tokens head_dim",
+            target_desc="num_tokens head_dim",
         ),
     ]
 
