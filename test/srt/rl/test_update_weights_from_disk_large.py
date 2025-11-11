e5901ac1c3cafca7d9eddd226a98b3a1276c4625
fzyzcjy <ch271828n@outlook.com>
2025-11-11 09:23:30 +0800

    more

--- test/srt/rl/test_update_weights_from_disk_large.py
+++ test/srt/rl/test_update_weights_from_disk_large.py
@@ -5,6 +5,7 @@ from test_update_weights_from_disk import TestServerUpdateWeightsFromDisk as _Pa
 
 class TestServerUpdateWeightsFromDiskDeepSeek(_ParentTest):
     model = "deepseek-ai/DeepSeek-V3-0324"
+    # TODO change to another model, o/w cannot test it really works
     model_after_update = "deepseek-ai/DeepSeek-V3-0324"
     launch_server_other_args = ["--tp", "4"]
 
