6f6db1b89c81465484d14a4a0610c491bc424b3e
fzyzcjy <ch271828n@outlook.com>
2025-11-11 17:50:16 +0800

    rm log

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -204,15 +204,6 @@ _is_cublas_ge_129 = is_nvidia_cublas_cu12_version_ge_12_9()
 logger = logging.getLogger(__name__)
 
 
-def get_tensor_info(x):
-    if not isinstance(x, torch.Tensor):
-        return f"type={type(x)} value={x}"
-    min = x.float().min() if x.numel() > 0 else None
-    max = x.float().max() if x.numel() > 0 else None
-    mean = x.float().mean() if x.numel() > 0 else None
-    return f"shape={x.shape} dtype={x.dtype} device={x.device} stride={x.stride()} req_grad={x.requires_grad} min={min} max={max} mean={mean}"
-
-
 def enable_nextn_moe_bf16_cast_to_fp8(quant_config):
     return (
         quant_config is not None
@@ -3139,20 +3130,6 @@ class DeepseekV2ForCausalLM(nn.Module):
         return self.model.end_layer
 
     def post_load_weights(self, is_nextn=False, weight_names=None):
-        if torch.distributed.get_rank() == 0:
-            print(
-                f"hi post_load_weights START {[(name, get_tensor_info(x)) for name, x in self.named_parameters()]=}"
-            )
-            a = self.model.layers[0].self_attn
-            print(
-                f"hi layer0-selfattn at-START "
-                f"{get_tensor_info(a.w_kc)=} "
-                f"{get_tensor_info(a.w_vc)=} "
-                f"{get_tensor_info(a.w_scale)=} "
-                f"{get_tensor_info(a.w_scale_k)=} "
-                f"{get_tensor_info(a.w_scale_v)=} "
-                f"{get_tensor_info(a.use_deep_gemm_bmm)=} "
-            )
 
         # Perform post-processing after loading weights
         if is_nextn:
@@ -3362,21 +3339,6 @@ class DeepseekV2ForCausalLM(nn.Module):
         if is_nextn and enable_nextn_moe_bf16_cast_to_fp8(self.quant_config):
             self._transform_scale_nextn_moe_ue8m0()
 
-        if torch.distributed.get_rank() == 0:
-            print(
-                f"hi post_load_weights END {[(name, get_tensor_info(x)) for name, x in self.named_parameters()]=}"
-            )
-            a = self.model.layers[0].self_attn
-            print(
-                f"hi layer0-selfattn at-END "
-                f"{get_tensor_info(a.w_kc)=} "
-                f"{get_tensor_info(a.w_vc)=} "
-                f"{get_tensor_info(a.w_scale)=} "
-                f"{get_tensor_info(a.w_scale_k)=} "
-                f"{get_tensor_info(a.w_scale_v)=} "
-                f"{get_tensor_info(a.use_deep_gemm_bmm)=} "
-            )
-
     def _weight_requant_ue8m0(self, is_nextn=False):
         weight_block_size = self.quant_config.weight_block_size
 
@@ -3571,8 +3533,6 @@ class DeepseekV2ForCausalLM(nn.Module):
             params_dict = dict(self.named_parameters())
             weight_names = []
             for name, loaded_weight in weights:
-                if torch.distributed.get_rank() == 0:
-                    print(f"hi load_weights {name=} {get_tensor_info(loaded_weight)=}")
                 use_async_loading = should_async_load(loaded_weight)
                 layer_id = get_layer_id(name)
                 if (
