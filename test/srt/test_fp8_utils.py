dd8317d678011071f3cc2610366e824fd20a2363
fzyzcjy <ch271828n@outlook.com>
2025-11-11 16:32:29 +0800

    more

---
+++ test/srt/test_fp8_utils.py
@@ -0,0 +1,16 @@
+import unittest
+
+from sglang.srt.parser.jinja_template_utils import (
+    detect_jinja_template_content_format,
+    process_content_for_template_format,
+)
+from sglang.test.test_utils import CustomTestCase
+
+
+class TestInverseTransformScaleUe8m0(CustomTestCase):
+    def test_round_trip(self):
+        TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
