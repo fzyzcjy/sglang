c075fc9887beec4628944fb4ba4cb7bfc046587e
fzyzcjy <ch271828n@outlook.com>
2025-11-11 17:07:46 +0800

    more

--- test/srt/test_fp8_utils.py
+++ test/srt/test_fp8_utils.py
@@ -9,17 +9,24 @@ from python.sglang.srt.layers.quantization.fp8_utils import transform_scale_ue8m
 
 class TestInverseTransformScaleUe8m0(CustomTestCase):
     def test_round_trip(self):
-        weight_bf16 = TODO
-        weight_block_size = [128, 128]
+        for _ in range(100):
+            weight_bf16 = torch.randn(
+                # DeepSeek V3 kv_b_proj
+                (32768, 512),
+                dtype=torch.bfloat16,
+                device="cuda",
+            )
 
-        qweight, sf_fp32 = quant_weight_ue8m0(weight_bf16, weight_block_size=weight_block_size)
-        mn = qweight.shape[-2]
+            weight_block_size = [128, 128]
 
-        sf_packed = transform_scale_ue8m0(sf_fp32, mn=mn)
-        sf_fp32 = inverse_transform_scale_ue8m0(sf_packed, mn=mn)
+            qweight, sf_fp32 = quant_weight_ue8m0(weight_bf16, weight_block_size=weight_block_size)
+            mn = qweight.shape[-2]
 
-        sf_packed_recreated = transform_scale_ue8m0(sf_fp32, mn=mn)
-        assert torch.all(sf_packed == sf_packed_recreated), f"{sf_packed=} {sf_packed_recreated}"
+            sf_packed = transform_scale_ue8m0(sf_fp32, mn=mn)
+            sf_fp32 = inverse_transform_scale_ue8m0(sf_packed, mn=mn)
+
+            sf_packed_recreated = transform_scale_ue8m0(sf_fp32, mn=mn)
+            assert torch.all(sf_packed == sf_packed_recreated), f"{sf_packed=} {sf_packed_recreated}"
 
 
 if __name__ == "__main__":
