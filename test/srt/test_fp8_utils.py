c2965788681d4eee755717e6e9dedfe7109c6817
fzyzcjy <ch271828n@outlook.com>
2025-11-11 16:33:31 +0800

    more

--- test/srt/test_fp8_utils.py
+++ test/srt/test_fp8_utils.py
@@ -1,9 +1,5 @@
 import unittest
 
-from sglang.srt.parser.jinja_template_utils import (
-    detect_jinja_template_content_format,
-    process_content_for_template_format,
-)
 from sglang.test.test_utils import CustomTestCase
 
 
