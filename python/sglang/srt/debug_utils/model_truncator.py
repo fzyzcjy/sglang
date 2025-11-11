d1d37e1071aac889701e6fdda92f45129c8dfe48
fzyzcjy <ch271828n@outlook.com>
2025-11-11 14:11:27 +0800

    more

--- python/sglang/srt/debug_utils/model_truncator.py
+++ python/sglang/srt/debug_utils/model_truncator.py
@@ -1,9 +1,13 @@
 # This file also references Slime :: fp8_cast_bf16.py
 
+import torch
 import json
 import os
 from pathlib import Path
 from argparse import ArgumentParser
+from typing import Dict
+
+from safetensors.torch import load_file, save_file
 
 
 def main(args):
@@ -16,15 +20,22 @@ def main(args):
         os.system(f"cp -rf {input_path}/{pattern} {output_path}")
 
     filename_index = "model.safetensors.index.json"
-    tensor_index = json.loads((input_path / filename_index).read_text())
-    tensor_index = _transform_index(tensor_index)
-    (output_path / filename_index).write_text(json.dumps(tensor_index, indent=4))
+    safetensors_index = json.loads((input_path / filename_index).read_text())
+    _transform_index(safetensors_index)
+    (output_path / filename_index).write_text(json.dumps(safetensors_index, indent=4))
 
+    for path_safetensor in sorted(list(input_path.glob("*.safetensors"))):
+        state_dict = load_file(path_safetensor)
+        _transform_safetensor_file(state_dict, safetensors_index)
+        save_file(state_dict, output_path / path_safetensor.relative_to(input_path))
+
+
+def _transform_index(safetensors_index):
     TODO
 
 
-def _transform_index(src):
-    return TODO
+def _transform_safetensor_file(state_dict: Dict[str, torch.Tensor], safetensors_index: Dict[str, str]):
+    TODO
 
 
 if __name__ == "__main__":
