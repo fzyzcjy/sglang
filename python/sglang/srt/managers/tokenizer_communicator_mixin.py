d38de5c65cd4b14ce18b6a14895b4c37e72593f6
fzyzcjy <ch271828n@outlook.com>
2025-11-29 17:26:18 +0800

    more

--- python/sglang/srt/managers/tokenizer_communicator_mixin.py
+++ python/sglang/srt/managers/tokenizer_communicator_mixin.py
@@ -325,6 +325,7 @@ class TokenizerCommunicatorMixin:
         profile_by_stage: bool = False,
         merge_profiles: bool = False,
         profile_prefix: Optional[str] = None,
+        profile_stages: Optional[List[str]] = None,
     ):
         self.auto_create_handle_loop()
         env_with_stack: bool = get_bool_env_var("SGLANG_PROFILE_WITH_STACK", "true")
