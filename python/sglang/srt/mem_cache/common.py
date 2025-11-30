52d671d5ef292fc7ff18e87ef6766715e66d73e4
fzyzcjy <ch271828n@outlook.com>
2025-11-30 21:49:51 +0800

    Revert "hack: evict much more"
    This reverts commit 33665558280e47ce9fe57d464614be53cb91d120.

--- python/sglang/srt/mem_cache/common.py
+++ python/sglang/srt/mem_cache/common.py
@@ -262,8 +262,6 @@ def alloc_paged_token_slots_extend(
     # Over estimate the number of tokens: assume each request needs a new page.
     allocator = tree_cache.token_to_kv_pool_allocator
     num_tokens = extend_num_tokens + len(seq_lens_cpu) * allocator.page_size
-    print(f"hack: evict much more!! original {num_tokens=}")
-    num_tokens = num_tokens * 4
     evict_from_tree_cache(tree_cache, num_tokens)
 
     state = None
