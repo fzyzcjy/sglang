f10e92a4891e52d226758022c3b4a8295c6d44e1
fzyzcjy <ch271828n@outlook.com>
2025-11-30 11:16:28 +0800

    more

--- python/sglang/srt/utils/numa_utils.py
+++ python/sglang/srt/utils/numa_utils.py
@@ -9,14 +9,14 @@ def configure_subprocess(server_args: ServerArgs, gpu_id: int):
     if (numa_nodes := server_args.numa_node) is not None and envs.SGLANG_NUMA_BIND_V2.get():
         numa_node = numa_nodes[gpu_id]
         numactl_args = f"--cpunodebind={numa_node} --membind={numa_node}"
-        with _configure_subprocess_impl(numactl_args=numactl_args):
+        with _configure_subprocess_numactl(numactl_args=numactl_args):
             pass
     else:
         yield
 
 
 @contextmanager
-def _configure_subprocess_impl(numactl_args: str):
+def _configure_subprocess_numactl(numactl_args: str):
     TODO
     try:
         yield
