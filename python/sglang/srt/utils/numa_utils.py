5270bdf185319fea90772fb535e64fb7275715b4
fzyzcjy <ch271828n@outlook.com>
2025-11-30 11:15:45 +0800

    more

--- python/sglang/srt/utils/numa_utils.py
+++ python/sglang/srt/utils/numa_utils.py
@@ -6,15 +6,17 @@ from sglang.srt.environ import envs
 
 @contextmanager
 def configure_subprocess(server_args: ServerArgs, gpu_id: int):
-    if (numa_node := server_args.numa_node) is not None and envs.SGLANG_NUMA_BIND_V2.get():
-        with _configure_subprocess_impl(numa_node=numa_node[gpu_id]):
+    if (numa_nodes := server_args.numa_node) is not None and envs.SGLANG_NUMA_BIND_V2.get():
+        numa_node = numa_nodes[gpu_id]
+        numactl_args = f"--cpunodebind={numa_node} --membind={numa_node}"
+        with _configure_subprocess_impl(numactl_args=numactl_args):
             pass
     else:
         yield
 
 
 @contextmanager
-def _configure_subprocess_impl(numa_node: int):
+def _configure_subprocess_impl(numactl_args: str):
     TODO
     try:
         yield
