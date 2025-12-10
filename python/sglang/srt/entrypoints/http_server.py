0aecbfb11d173018dee82876078d35db45d66790
fzyzcjy <ch271828n@outlook.com>
2025-12-10 20:36:09 +0800

    feat/engine_http

--- python/sglang/srt/entrypoints/http_server.py
+++ python/sglang/srt/entrypoints/http_server.py
@@ -122,6 +122,7 @@ from sglang.srt.server_args import PortArgs, ServerArgs
 from sglang.srt.utils import (
     add_api_key_middleware,
     add_prometheus_middleware,
+    add_prometheus_track_response_middleware,
     delete_directory,
     get_bool_env_var,
     kill_process_tree,
@@ -1393,6 +1394,9 @@ def launch_server(
             scheduler_info=scheduler_info,
         )
     )
+
+    if server_args.enable_metrics:
+        add_prometheus_track_response_middleware(app)
 
     # Pass additional arguments to the lifespan function.
     # They will be used for additional initialization setups.
