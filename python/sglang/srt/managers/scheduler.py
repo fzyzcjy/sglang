72c1f6bef31a287c332993477e47d96d438ab159
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-12-17 21:19:42 +0800

    Add cuda_graph_forward_passes_total and num_retracted_reqs_total (#15189)
    (cherry picked from commit b12b40dea4bc3e64da304c747947d4f3457a782f)

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1917,6 +1917,8 @@ class Scheduler(
                 self.server_args
             )
             self.num_retracted_reqs = len(retracted_reqs)
+            if self.enable_metrics and (x := len(retracted_reqs)) > 0:
+                self.metrics_collector.increment_num_retracted_reqs(x)
             self.new_token_ratio = new_token_ratio
             for req in reqs_to_abort:
                 abort_reason: FINISH_ABORT = req.to_finish
