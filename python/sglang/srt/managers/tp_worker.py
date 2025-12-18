e791040378e3e6a9268acd8427d5b4d0c216cddd
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-12-18 22:24:23 +0800

    Support EPLB balancedness prometheus metric without GPU->CPU synchronize (#15401)
    (cherry picked from commit 88a405cc10c5ad5a7882df85c802ac98278fe2cb)

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -398,6 +398,7 @@ class TpModelWorker(BaseTpWorker):
             batch_result = GenerationBatchResult(
                 logits_output=logits_output,
                 can_run_cuda_graph=can_run_cuda_graph,
+                expert_distribution_metrics=out.expert_distribution_metrics,
             )
 
             if is_verify:
@@ -451,6 +452,7 @@ class TpModelWorker(BaseTpWorker):
             return GenerationBatchResult(
                 pp_hidden_states_proxy_tensors=pp_proxy_tensors,
                 can_run_cuda_graph=can_run_cuda_graph,
+                expert_distribution_metrics=out.expert_distribution_metrics,
             )
 
     def forward_batch_split_prefill(self, batch: ScheduleBatch):
@@ -473,6 +475,7 @@ class TpModelWorker(BaseTpWorker):
         batch_result = GenerationBatchResult(
             logits_output=logits_output,
             can_run_cuda_graph=can_run_cuda_graph,
+            expert_distribution_metrics=out.expert_distribution_metrics,
         )
         batch_result.next_token_ids = next_token_ids
         return batch_result
