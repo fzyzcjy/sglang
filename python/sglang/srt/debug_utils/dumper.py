852a85ea9628d1fd9ea327095eb8cc90ce81b43b
fzyzcjy <ch271828n@outlook.com>
2026-02-15 21:08:10 +0800

    more

--- python/sglang/srt/debug_utils/dumper.py
+++ python/sglang/srt/debug_utils/dumper.py
@@ -45,7 +45,7 @@ class _Dumper:
         self._base_dir = Path(os.environ.get("SGLANG_DUMPER_DIR", "/tmp"))
         self._enable_write_file = get_bool_env_var("SGLANG_DUMPER_WRITE_FILE", "1")
         self._enable_dump_forward = get_bool_env_var("SGLANG_DUMPER_DUMP_FORWARD", "1")
-        self._enable_dump_grad = get_bool_env_var("SGLANG_DUMPER_DUMP_GRAD", "1")
+        self._enable_dump_grad = get_bool_env_var("SGLANG_DUMPER_DUMP_GRAD", "0")
         self._output_dict_mode = get_bool_env_var("SGLANG_DUMPER_OUTPUT_DICT", "0")
 
         # States
