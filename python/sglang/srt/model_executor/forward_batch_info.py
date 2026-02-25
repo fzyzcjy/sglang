e54926fcb86f6f9e52fb8eaadf6806126533b8dd
fzyzcjy <ch271828n@outlook.com>
2026-02-25 20:22:10 +0800

    more

--- python/sglang/srt/model_executor/forward_batch_info.py
+++ python/sglang/srt/model_executor/forward_batch_info.py
@@ -420,7 +420,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
             tbo_split_seq_index=batch.tbo_split_seq_index,
             dimensions=batch.dimensions,
             return_hidden_states_before_norm=batch.return_hidden_states_before_norm,
-            rids=batch.rids,
+            rids=[req.rid for req in batch.reqs],
         )
         device = model_runner.device
 
